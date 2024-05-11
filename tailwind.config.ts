1ef1b61c5fe90686e65ac997890503a4460b7cce
Abhay Desai <abhaydesai110@gmail.com>
2024-05-11 12:39:44 +0530

    tailwind setup done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,80 +1,87 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "2k": "1920px",
+      "4k": "2560px",
+    },
     container: {
       center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
+      padding: {
+        DEFAULT: "1.25rem",
+        md: "1.75rem",
+        lg: "0.875rem",
+        xl: "2.25rem",
+        "2xl": "3rem",
+        "2k": "1rem",
       },
+    },
+    fontFamily: {
+      jos: ["Josefin Sans", "sans-serif"],
+      pop: ["Poppins", "sans-serif"],
+    },
+    fontSize: {
+      xs: [".75rem", "1.1rem"],
+      sm: [".875rem", "1.356rem"],
+      base: ["1rem", "1.45rem"],
+      lg: ["1.125rem", "1.75rem"],
+      xl: ["1.25rem", "1.875rem"],
+      "22": ["1.375rem", "2.063rem"],
+      "28": ["1.75rem", "2.375rem"],
+      "2xl": ["1.5rem", "2.25rem"],
+      "26": ["1.625rem", "2.25rem"],
+      "3xl": ["1.875rem", "2.5rem"],
+      "4xl": ["2.25rem", "2.75rem"],
+      "5xl": ["3rem", "3.75rem"],
+      "6xl": ["3.75rem", "5rem"],
+      "7xl": ["4.375rem", "5.625rem"],
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+        goldie: {
+          primary: {
+            DEFAULT: "#0B2D61",
+          },
+          secondary: {
+            DEFAULT: "#EE3139",
+          },
+          dark: "#01183A",
+          darkPlus: "#021C44",
+          gray: "#444444",
+          grayLight: "#D9D9D9",
+          ceil: "#8ba1c4",
+          jpIndigo: "#283a56",
         },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+      boxShadow: {
+        one: "0px 0px 20px #0000000D",
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        zoom: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.05)" },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        zoom: "zoom 1s ease-in-out infinite",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
